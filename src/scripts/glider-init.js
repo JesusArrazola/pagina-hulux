ac45d568658049ca97cf6a08275a70a6b2108876
Jesus Arrazola <arrazola69@hotmail.com>
2025-04-23 13:36:11 -0600

    migracion a SliderReutilizable en Servicios

--- src/scripts/glider-init.js
+++ src/scripts/glider-init.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
   const el = document.querySelector('.glider');
+  const testimonial = document.querySelector(".testimonial");
   if (el) {
     new Glider(el, {
       slidesToShow: "auto",           // Móvil por defecto
@@ -19,6 +20,39 @@ window.addEventListener('DOMContentLoaded', () => {
           },
         },
       ],
+    });
+  }
+
+  
+  //Testimonial
+  if(testimonial){
+    new Glider(testimonial, {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      draggable: true,
+      dots: '.t-dots',
+      responsive: [
+        {
+          // lg: Tailwind = 1024px
+          breakpoint: 1280,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            itemWidth: undefined,
+            exactWidth: false,
+          },
+        },
+        {
+          // lg: Tailwind = 1024px
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            itemWidth: undefined,
+            exactWidth: false,
+          },
+        },
+      ]
     });
   }
 });
