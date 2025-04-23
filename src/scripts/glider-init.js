d01ed97f6c090f0fe104e02b7c7b96c6caaf8b5d
Jesus Arrazola <arrazola69@hotmail.com>
2025-04-23 15:56:15 -0600

    Testimonio de clientes añadido

--- src/scripts/glider-init.js
+++ src/scripts/glider-init.js
@@ -1,30 +1,21 @@
 window.addEventListener('DOMContentLoaded', () => {
   const testimonial = document.querySelector(".testimonial");
+  console.log(testimonial)
   const test = document.querySelector(".testt");
-  //Testimonial
-  if(test){
-    new Glider(test, {
+
+  if(testimonial){
+    new Glider(testimonial, {
       slidesToShow: 1,
       slidesToScroll: 1,
       draggable: true,
-      dots: '.t2-dots',
+      dots: '.t-dots',
       responsive: [
-        {
-          // lg: Tailwind = 1024px
-          breakpoint: 1280,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            itemWidth: undefined,
-            exactWidth: false,
-          },
-        },
         {
           // lg: Tailwind = 1024px
           breakpoint: 1024,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
+            slidesToShow: 3,
+            slidesToScroll: 3,
             itemWidth: undefined,
             exactWidth: false,
           },
@@ -34,28 +25,19 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   if(test){
-    new Glider(testimonial, {
+    new Glider(test, {
+      rewind: true,
       slidesToShow: 1,
       slidesToScroll: 1,
       draggable: true,
-      dots: '.t-dots',
+      dots: '.tst-dots',
       responsive: [
-        {
-          // lg: Tailwind = 1024px
-          breakpoint: 1280,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            itemWidth: undefined,
-            exactWidth: false,
-          },
-        },
         {
           // lg: Tailwind = 1024px
           breakpoint: 1024,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
+            slidesToShow: 3,
+            slidesToScroll: 3,
             itemWidth: undefined,
             exactWidth: false,
           },
