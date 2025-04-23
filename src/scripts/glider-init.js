110b4c6da800c2253d6971e955e9bb997a8a8cb9
Jesus Arrazola <arrazola69@hotmail.com>
2025-04-23 15:30:03 -0600

    separacion de componentes

--- src/scripts/glider-init.js
+++ src/scripts/glider-init.js
@@ -1,31 +1,39 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const el = document.querySelector('.glider');
   const testimonial = document.querySelector(".testimonial");
-  if (el) {
-    new Glider(el, {
-      slidesToShow: "auto",           // Móvil por defecto
+  const test = document.querySelector(".testt");
+  //Testimonial
+  if(test){
+    new Glider(test, {
+      slidesToShow: 1,
       slidesToScroll: 1,
-      itemWidth: 345,            // Ancho fijo para modo móvil
-      exactWidth: true,
       draggable: true,
+      dots: '.t2-dots',
       responsive: [
         {
           // lg: Tailwind = 1024px
           breakpoint: 1280,
           settings: {
             slidesToShow: 3,
-            slidesToScroll: 1,
+            slidesToScroll: 3,
             itemWidth: undefined,
             exactWidth: false,
           },
         },
-      ],
+        {
+          // lg: Tailwind = 1024px
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            itemWidth: undefined,
+            exactWidth: false,
+          },
+        },
+      ]
     });
   }
 
-  
-  //Testimonial
-  if(testimonial){
+  if(test){
     new Glider(testimonial, {
       slidesToShow: 1,
       slidesToScroll: 1,
