170e83c53db382ed80dcb8b80d95318040fd2830
Jesus Arrazola <arrazola69@hotmail.com>
2025-04-23 08:12:12 -0600

    Prueba carrusel

--- src/scripts/glider-init.js
+++ src/scripts/glider-init.js
@@ -1,19 +1,21 @@
-import Glider from 'glider-js';
-
 window.addEventListener('DOMContentLoaded', () => {
   const el = document.querySelector('.glider');
   if (el) {
     new Glider(el, {
-      slidesToShow: 1,
+      slidesToShow: 1,           // Móvil por defecto
       slidesToScroll: 1,
       draggable: true,
+      itemWidth: 385,            // Ancho fijo para modo móvil
+      exactWidth: true,
       responsive: [
         {
-          breakpoint: 768,
+          // lg: Tailwind = 1024px
+          breakpoint: 1024,
           settings: {
-            slidesToShow: "auto",
+            slidesToShow: 3,
             slidesToScroll: 1,
-            itemWidth: 385,
+            itemWidth: undefined,
+            exactWidth: false,
           },
         },
       ],
