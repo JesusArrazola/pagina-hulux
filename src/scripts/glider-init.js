a25f0df66a2e4bc75c22d0c4f9f3fa589c39ed8b
Jesus Arrazola <arrazola69@hotmail.com>
2025-04-25 16:30:15 -0600

    Componente de Precio

--- src/scripts/glider-init.js
+++ src/scripts/glider-init.js
@@ -1,7 +1,49 @@
 window.addEventListener('DOMContentLoaded', () => {
   const testimonial = document.querySelector(".testimonial");
-  console.log(testimonial)
   const test = document.querySelector(".testt");
+
+  const residenciales = document.querySelector(".residenciales");
+
+  if(residenciales){
+    new Glider(residenciales, {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      draggable: true,
+      dots: '.sr-dots',
+      responsive: [
+        {
+          // lg: Tailwind = 1024px
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            itemWidth: undefined,
+            exactWidth: false,
+          },
+        },
+        {
+          // lg: Tailwind = 1024px
+          breakpoint: 1210,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            itemWidth: undefined,
+            exactWidth: false,
+          },
+        },
+        {
+          // lg: Tailwind = 1024px
+          breakpoint: 1550,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            itemWidth: undefined,
+            exactWidth: false,
+          },
+        },
+      ]
+    });
+  }
 
   if(testimonial){
     new Glider(testimonial, {
