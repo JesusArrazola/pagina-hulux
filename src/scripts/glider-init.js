cfeaae5b1cc47e7923bd97b523cb877a7b5773a8
Jesus Arrazola <arrazola69@hotmail.com>
2025-04-28 14:34:31 -0600

    Seccion servicios empresariales

--- src/scripts/glider-init.js
+++ src/scripts/glider-init.js
@@ -3,6 +3,98 @@ window.addEventListener('DOMContentLoaded', () => {
   const test = document.querySelector(".testt");
 
   const residenciales = document.querySelector(".residenciales");
+  const residencialesTV = document.querySelector(".residenciales-tv");
+  const empresariales = document.querySelector(".empresariales");
+
+  if(empresariales){
+    new Glider(empresariales, {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      draggable: true,
+      dots: 'emp-dots',
+      arrows:{
+        prev: ".emp-glider-prev",
+        next: ".emp-glider-next"
+      },
+      responsive: [
+        {
+          // lg: Tailwind = 1024px
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            itemWidth: undefined,
+            exactWidth: false,
+          },
+        },
+        {
+          // lg: Tailwind = 1024px
+          breakpoint: 1210,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            itemWidth: undefined,
+            exactWidth: false,
+          },
+        },
+        {
+          // lg: Tailwind = 1024px
+          breakpoint: 1550,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            itemWidth: undefined,
+            exactWidth: false,
+          },
+        },
+      ]
+    });
+  }
+
+  if(residencialesTV){
+    new Glider(residencialesTV, {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      draggable: true,
+      dots: '.srtv-dots',
+      arrows:{
+        prev: ".srtv-glider-prev",
+        next: ".srtv-glider-next"
+      },
+      responsive: [
+        {
+          // lg: Tailwind = 1024px
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            itemWidth: undefined,
+            exactWidth: false,
+          },
+        },
+        {
+          // lg: Tailwind = 1024px
+          breakpoint: 1210,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            itemWidth: undefined,
+            exactWidth: false,
+          },
+        },
+        {
+          // lg: Tailwind = 1024px
+          breakpoint: 1550,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            itemWidth: undefined,
+            exactWidth: false,
+          },
+        },
+      ]
+    });
+  }
 
   if(residenciales){
     new Glider(residenciales, {
